0de4b21573e2759e9462c061b18003017c765c56
toshiki <pcdjha@gmail.com>
2019-07-12 23:25:28 +0900

    add

--- src/utility/cmn_cmx.cpp
+++ src/utility/cmn_cmx.cpp
@@ -1,4 +1,4 @@
 ﻿template<typename T, typename U>
-inline auto& cmn (T& a, U b) {return a = a <= b ? a : b;}
+inline auto& cmn (T& a, U b) {if (a > b) {a = b; return true;} return false;}
 template<typename T, typename U>
-inline auto& cmx (T& a, U b) {return a = a >= b ? a : b;}
+inline auto& cmx (T& a, U b) {if (a < b) {a = b; return true;} return false;}
