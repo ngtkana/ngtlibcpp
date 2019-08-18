4261e0fc91535bba953e19595ba45bd4a25a274f
ngtkana <ngtkana@gmail.com>
2019-08-18 20:04:57 +0900

    fixed bipartite matching

--- include/network_flow/bipartite_matching.hpp
+++ include/network_flow/bipartite_matching.hpp
@@ -1,17 +1,93 @@
-class bipartite_matching {
-  int x, y;
-  dinic<int> dnc;
-  public:
-    bipartite_matching(int x, int y) :
-      x(x), y(y), dnc([x, y]{
-        dinic<int> dnc(1 + x + y + 1, 0, 1 + x + y);
-        for (int i = 0; i < x; i++) dnc.insert(0, 1 + i, 1);
-        for (int j = 0; j < y; j++) dnc.insert(1 + x + j, 1 + x + y, 1);
-        return dnc;
-      }())
-      {}
-    void insert (int i, int j) {
-      dnc.insert(1 + i, 1 + x + j, 1);
+class bipartite_matching
+{
+    int L, R;
+    std::vector<std::vector<int>> graph;
+    std::vector<int> match_list, level;
+
+    bool bfs()
+    {
+      std::fill(level.begin(), level.end(), -1);
+      std::queue<int> que;
+      for (int i = 0; i < L; i++)
+      {
+        if (match_list.at(i) < 0)
+        {
+          level.at(i) = 0;
+          que.emplace(i);
+        }
+      }
+      bool ret = false;
+      while (!que.empty())
+      {
+        auto crr = que.front(); que.pop();
+        for (auto nxt : graph.at(crr))
+        {
+          if (level.at(nxt) != -1) continue;
+          level.at(nxt) = level.at(crr) + 1;
+          auto w = match_list.at(nxt);
+          if (w == -1)
+          {
+            ret = true;
+          }
+          else if (level.at(w) == -1)
+          {
+            level.at(w) = level.at(crr) + 1;
+            que.emplace(w);
+          }
+        }
+      }
+      return ret;
     }
-    int cal () {return dnc.cal();}
+
+    bool dfs(int crr)
+    {
+      for (auto nxt : graph.at(crr))
+      {
+        assert(level.at(nxt) <= level.at(crr) + 1);
+        if (level.at(nxt) < level.at(crr) + 1) continue;
+        level.at(nxt) = -1;
+        auto w = match_list.at(nxt);
+        if (w == -1 || dfs(w))
+        {
+          match_list.at(nxt) = crr;
+          match_list.at(crr) = nxt;
+          level.at(crr) = -1;
+          return true;
+        }
+      }
+      level.at(crr) = -1;
+      return false;
+    }
+
+  public:
+    bipartite_matching()=default;
+    bipartite_matching(int L, int R):
+      L(L), R(R), graph(L + R), match_list(L + R, -1), level(L + R)
+      {}
+
+    void insert_edge(int u, int v)
+    {
+      v += L;
+      graph.at(u).emplace_back(v);
+      graph.at(v).emplace_back(u);
+    }
+
+
+    int build ()
+    {
+      auto ret = 0;
+      while (bfs())
+      {
+        for (int i = 0; i < L; i++)
+        {
+          if (match_list.at(i) == -1 && dfs(i))
+          {
+            ret++;
+          }
+        }
+      }
+      return ret;
+    }
+
+    auto match() const {return match_list;}
 };
