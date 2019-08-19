eda239afe48f3430a245fba13a14863768c31893
ngtkana <ngtkana@gmail.com>
2019-08-19 23:36:19 +0900

    renamed

--- snippets/code_fragments/stemp.hpp
+++ snippets/code_fragments/stemp.hpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define loop(n) for (int ngtkana_is_genius = 0; ngtkana_is_genius < n; ngtkana_is_genius++)
 #define rep(i, begin, end) for(int i = int(begin); i < int(end); i++)
+#define lint long long
 
 int main()
 {
