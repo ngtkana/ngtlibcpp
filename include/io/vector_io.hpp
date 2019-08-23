92d652bac9c6ee584c8c3464ef2b754dd79365a9
ngtkana <ngtkana@gmail.com>
2019-08-24 02:16:16 +0900

    add: io

--- include/io/vector_io.hpp
+++ include/io/vector_io.hpp
@@ -4,3 +4,13 @@ std::istream& operator>> (std::istream& is, std::vector<T>& v)
   for (auto & x : v) is >> x;
   return is;
 }
+
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const std::vector<T>& v)
+{
+  auto n = v.size();
+  os << "{";
+  for (size_t i = 0; i < n; i++)
+    os << (i > 0 ? "," : "") << v[i];
+  return os << "}";
+}
---
+++ test/io-test/io-test.cpp
@@ -0,0 +1,57 @@
+#include <catch2/catch.hpp>
+#include <io/tuple_io.hpp>
+#include <io/pair_io.hpp>
+#include <io/vector_io.hpp>
+#include <sstream>
+#include <iterator>
+#include <iostream>
+
+TEST_CASE("tuple io", "[tuple_io]")
+{
+  std::stringstream ss;
+  ss << 10 << std::endl
+     << 'c' << std::endl;
+  std::tuple<int, char> tuple;
+  ss >> tuple;
+  REQUIRE(std::get<0>(tuple) == 10);
+  REQUIRE(std::get<1>(tuple) == 'c');
+  ss.str("");
+  ss.clear(std::stringstream::goodbit);
+  ss << tuple;
+  REQUIRE(ss.str() == "(10,c)");
+}
+
+TEST_CASE("pair io", "[pair_io]")
+{
+  std::stringstream ss;
+  ss << 10 << std::endl
+     << 'c' << std::endl;
+  std::pair<int, char> pair;
+  ss >> pair;
+  REQUIRE(pair.first  == 10);
+  REQUIRE(pair.second == 'c');
+  ss.str("");
+  ss.clear(std::stringstream::goodbit);
+  ss << pair;
+  REQUIRE(ss.str() == "(10,c)");
+}
+
+TEST_CASE("vector io", "[vector_io]")
+{
+  int n = 5;
+  std::stringstream ss;
+  for (auto i = 0; i < n; i++)
+  {
+    ss << 10 + i << std::endl;
+  }
+  std::vector<int> v(n);
+  ss >> v;
+  for (auto i = 0; i < n; i++)
+  {
+    REQUIRE(v.at(i) == 10 + i);
+  }
+  ss.str("");
+  ss.clear(std::stringstream::goodbit);
+  ss << v;
+  REQUIRE(ss.str() == "{10,11,12,13,14}");
+}
