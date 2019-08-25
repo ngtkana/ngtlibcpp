ea51f83127d48c361ad04ed685bd5fa5b254e268
ngtkana <ngtkana@gmail.com>
2019-08-26 00:54:02 +0900

    fixed quick_find

--- include/disjoint_sets/quick_find.hpp
+++ include/disjoint_sets/quick_find.hpp
@@ -1,39 +1,35 @@
 class quick_find
 {
   int                           n;
-  std::vector<int>              leader;
-  std::vector<std::vector<int>> members;
+  std::vector<int>              prt;
+  std::vector<std::vector<int>> child;
 
 public:
-  quick_find (int n) : n(n), leader(n), members(n)
+  quick_find (int n) : n(n), prt(n), child(n)
   {
-    std::iota(leader.begin(), leader.end(), 0);
+    std::iota(prt.begin(), prt.end(), 0);
     for (int i = 0; i < n; i++) {
-      members.at(i) = {i};
+      child.at(i) = {i};
     }
   }
 
-  auto collect ()             const {return leader;}
-
+  auto collect ()             const {return prt;}
   bool is_root (int x)        const {return find(x) == x;}
-
-  int  size    (int x)        const {return members.at(x).size();}
-
+  int  size    (int x)        const {return child.at(prt.at(x)).size();}
   bool same    (int x, int y) const {return find(x) == find(y);}
-
-  int  find    (int x)        const {return leader.at(x);}
+  int  find    (int x)        const {return prt.at(x);}
 
   // Returns `true` if x and y are newly connected.
   bool unite   (int x, int y)
   {
     if ((x = find(x)) == (y = find(y))) return false;
     if (size(x) > size(y)) std::swap(x, y);
-    for (auto child : members.at(x))
+    for (auto z : child.at(x))
     {
-      leader.at(child) = y;
-      members.at(y).push_back(child);
+      prt.at(z) = y;
+      child.at(y).push_back(z);
     }
-    decltype(members)::value_type{}.swap(members.at(x));
+    decltype(child)::value_type{}.swap(child.at(x));
     return true;
   }
 };
