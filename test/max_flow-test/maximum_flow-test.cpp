4b3af2f74c5b34adc8d7addec817f4e6728f6c7b
ngtkana <ngtkana@gmail.com>
2019-08-27 23:32:48 +0900

    add more tests for max_flow

--- test/max_flow-test/maximum_flow-test.cpp
+++ test/max_flow-test/maximum_flow-test.cpp
@@ -19,7 +19,7 @@ TEMPLATE_TEST_CASE("Maximum Flow Problems", "[dinic]",
   dinic_tag
 )
 {
-  SECTION("AOJ Sample")
+  SECTION(u8"AOJ Sample")
   {
     auto max_flow = maximum_flow_engine<TestType>(4, 0, 3);
     max_flow.insert(0, 1, 2);
@@ -29,7 +29,8 @@ TEMPLATE_TEST_CASE("Maximum Flow Problems", "[dinic]",
     max_flow.insert(2, 3, 2);
     REQUIRE(max_flow.build() == 3);
   }
-  SECTION("Kyushu Univ. H - Ofuro ha kimochi ii")
+
+  SECTION(u8"Kyushu Univ. H - お風呂は気持ちいい")
   {
     constexpr int inf = 1 << 30;
     auto max_flow = maximum_flow_engine<TestType>(6, 5, 0);
@@ -39,6 +40,43 @@ TEMPLATE_TEST_CASE("Maximum Flow Problems", "[dinic]",
     max_flow.insert(4, 2, 100);
     max_flow.insert(2, 1, 50);
     max_flow.insert(1, 0, 200);
+    REQUIRE(max_flow.build() == 50);
+  }
+
+  SECTION(u8"yukicoder 117 制作進行の宮森あおいです！")
+  {
+    constexpr int inf = 1 << 30;
+    auto max_flow = maximum_flow_engine<TestType>(14, 12, 13);
+    for (auto i = 0; i < 10; i++)
+    {
+      auto cap = (i + 1) * 2;
+      max_flow.insert(12, i, cap);
+      max_flow.insert(i, (i < 5 ? 10 : 11), inf);
+    }
+    max_flow.insert(10, 13, 50);
+    max_flow.insert(11, 13, 70);
+    REQUIRE(max_flow.build() == 100);
+  }
+
+  SECTION(u8"yukicoder No.654 Air E869120")
+  {
+    constexpr int linf = 1 << 30;
+    auto max_flow = maximum_flow_engine<TestType>(12, 10, 11);
+    max_flow.insert(0, 5, 40);
+    max_flow.insert(1, 8, 10);
+    max_flow.insert(2, 7, 20);
+    max_flow.insert(3, 8, 20);
+    max_flow.insert(4, 9, 30);
+    max_flow.insert(10, 0, linf);
+    max_flow.insert(10, 1, linf);
+    max_flow.insert(0, 1, linf);
+    max_flow.insert(5, 2, linf);
+    max_flow.insert(2, 3, linf);
+    max_flow.insert(8, 7, linf);
+    max_flow.insert(7, 4, linf);
+    max_flow.insert(9, 8, linf);
+    max_flow.insert(9, 11, linf);
+    max_flow.insert(8, 11, linf);
     REQUIRE(max_flow.build() == 50);
   }
 }
