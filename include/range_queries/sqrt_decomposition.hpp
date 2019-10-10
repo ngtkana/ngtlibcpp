81492eb79fb2332b0d8d04a0373c93b2dda6dd8b
ngtkana <toshiki_tanaka@caddi.jp>
2019-10-10 23:15:32 +0900

    added sqrt_decomposition

---
+++ include/range_queries/sqrt_decomposition.hpp
@@ -0,0 +1,28 @@
+class sqrt_decomposition {
+  public:
+    int n, block_size, block_number;
+
+    sqrt_decomposition(int n, int block_size) :
+      n(n),
+      block_size(block_size),
+      block_number((n + block_size - 1) / block_size)
+      {}
+
+    auto get_block_size()   const { return block_size;   }
+    auto get_block_number() const { return block_number; }
+
+    template < class Append, class Stamp >
+    void query(int l, int r, Append const & append, Stamp const & stamp) {
+      while (l % block_size && l < r) {
+        append(l, l / block_size);
+        l++;
+      }
+      while (r % block_size && l < r) {
+        r--;
+        append(r, r / block_size);
+      }
+      for (auto j = l / block_size; j < r / block_size; j++) {
+        stamp(j);
+      }
+    }
+};
---
+++ test/range_queries/sqrt_decomposition.cpp
@@ -0,0 +1,85 @@
+#include <catch2/catch.hpp>
+#include <bits/stdc++.h>
+#include <range_queries/sqrt_decomposition.hpp>
+#define loop(n) for (int ngtkana_is_genius = 0; ngtkana_is_genius < int(n); ngtkana_is_genius++)
+#define rep(i, begin, end) for(int i = int(begin); i < int(end); i++)
+#define all(v) v.begin(), v.end()
+#define rand(l, r) std::uniform_int_distribution<>(l, r)(mt)
+using lint = long long;
+auto cmn = [](auto& a, auto b){if (a > b) {a = b; return true;} return false;};
+auto cmx = [](auto& a, auto b){if (a < b) {a = b; return true;} return false;};
+void debug_impl() { std::cerr << std::endl; }
+template <typename Head, typename... Tail>
+void debug_impl(Head head, Tail... tail){
+  std::cerr << " " << head;
+  debug_impl(tail...);
+}
+#ifndef STOPIT
+#define debug(...)\
+  std::cerr << std::boolalpha << "[" << #__VA_ARGS__ << "]:";\
+  debug_impl(__VA_ARGS__);\
+  std::cerr << std::noboolalpha;
+#else
+#define debug 0;
+#endif
+
+template < typename Container, typename Value = typename Container::value_type, std::enable_if_t<!std::is_same< Container, std::string >::value, std::nullptr_t> = nullptr>
+std::istream& operator>> (std::istream& is, Container& v)
+  { for (auto & x : v) { is >> x; } return is; }
+
+template < typename Container, typename Value = typename Container::value_type, std::enable_if_t<!std::is_same< Container, std::string >::value, std::nullptr_t> = nullptr >
+std::ostream& operator<< (std::ostream& os, Container const& v) {
+ os << "{";
+  for (auto it = v.begin(); it != v.end(); it++)
+    {os << (it != v.begin() ? "," : "") << *it;}
+  return os << "}";
+}
+
+auto mt = std::mt19937_64(std::random_device{}());
+TEMPLATE_TEST_CASE( "Sqrt Decomposition", "[sqrt_decomposition]", int, long long ) {
+  constexpr auto inf = std::numeric_limits< TestType >::max();
+  loop(24) {
+    auto n = rand(2, 40);
+    std::vector< TestType > a(n);
+    for (auto & x : a) x = std::uniform_int_distribution< TestType >(0, inf)(mt);
+
+    // prepare sqrt_decompositon
+    auto sd = sqrt_decomposition(n, std::sqrt(n));
+    auto m  = sd.get_block_number();
+    auto sz = sd.get_block_size();
+    std::vector< TestType > range_min(m, inf);
+    rep(i, 0, n)
+      { cmn(range_min.at(i / sz), a.at(i)); }
+
+    // query
+    loop(100) {
+      // update
+      if (rand(0, 1)) {
+        auto i = rand(0, n - 1);
+        auto x = std::uniform_int_distribution< TestType >(0, inf)(mt);
+        a.at(i) = x;
+        auto j = i / sz;
+        auto & crr = range_min.at(j);
+        crr = inf;
+        rep(k, j * sz, std::min(n, (j + 1) * sz))
+          { cmn(crr, a.at(k)); }
+      }
+
+      // range-min
+      else {
+        auto l = rand(0, n - 2), r = rand(0, n - 2);
+        if (r < l) std::swap(l, r);
+        r++;
+        auto ret0 = inf;
+        sd.query(l, r,
+          [&](auto i, auto){ cmn(ret0, a.at(i)); },
+          [&](auto j){ cmn(ret0, range_min.at(j)); }
+        );
+        auto ret1 = inf;
+        rep(i, l, r)
+          { cmn(ret1, a.at(i)); }
+        REQUIRE(ret0 == ret1);
+      }
+    }
+  }
+}
