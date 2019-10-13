1edf3d99b20f4d26879c444b9472cd9cefd1eb89
ngtkana <toshiki_tanaka@caddi.jp>
2019-10-13 23:29:51 +0900

    fixed polynominal

--- include/semirings/polynominal.hpp
+++ include/semirings/polynominal.hpp
@@ -1,17 +1,34 @@
-template < class Mul, class Add, class Value >
+
+template < class Mul, class Add, class Value, class Div, class Sub >
 class polynominal {
     using poly_type = std::vector< Value >;
     Mul mul_fn;
     Add add_fn;
     Value zero;
+    Div div_fn;
+    Sub sub_fn;
     auto add_eq(Value & x, Value y) const { x = add_fn(x, y); }
+    auto sub_eq(Value & x, Value y) const { x = sub_fn(x, y); }
 
   public:
     polynominal()=default;
-    polynominal(Mul&& mul_fn, Add&& add_fn, Value zero) :
-      mul_fn(std::forward< Mul >(mul_fn)),
-      add_fn(std::forward< Add >(add_fn)),
-      zero(zero){}
+    polynominal(
+      Mul const& mul_fn,
+      Add const& add_fn,
+      Value zero,
+      Div const& div_fn,
+      Sub const& sub_fn
+    ) :
+      mul_fn(mul_fn), add_fn(add_fn),
+      zero(zero),
+      div_fn(div_fn), sub_fn(sub_fn)
+      {}
+
+    auto& normalize(poly_type & a) const {
+      while (!a.empty() && a.back() == zero)
+        { a.pop_back(); }
+      return a;
+    }
 
     auto add(poly_type const & a , poly_type const & b) const {
       int l = a.size(), m = b.size();
@@ -21,7 +38,7 @@ class polynominal {
         if (i < l) add_eq(c.at(i), a.at(i));
         if (i < m) add_eq(c.at(i), b.at(i));
       }
-      return c;
+      return normalize(c);
     }
     auto mul(poly_type const & a, poly_type const & b) const {
       int l = a.size(), m = b.size();
@@ -32,9 +49,50 @@ class polynominal {
           add_eq(c.at(i + j), mul_fn(a.at(i), b.at(j)));
         }
       }
-      return c;
+      return normalize(c);
+    }
+    auto sub(poly_type const & a , poly_type const & b) const {
+      int l = a.size(), m = b.size();
+      int n = std::max(a.size(), b.size());
+      poly_type c(n, zero);
+      for (auto i = 0; i < n; i++) {
+        if (i < l) add_eq(c.at(i), a.at(i));
+        if (i < m) sub_eq(c.at(i), b.at(i));
+      }
+      return normalize(c);
+    }
+    auto div(poly_type a, poly_type b) const {
+      normalize(a), normalize(b);
+      assert(!b.empty());
+      int l = a.size(), m = b.size();
+      if (l < m)
+        { return std::make_pair(poly_type{}, a); }
+      poly_type q(l - m + 1, zero);
+      while (m <= l) {
+        auto d = l - m;
+        auto x = div_fn(a.back(), b.back());
+        q.at(d) = x;
+        for (auto i = 0; i < m; i++)
+          { sub_eq(a.at(i + d), x * b.at(i)); }
+        normalize(a);
+        assert((int)a.size() < l);
+        l = a.size();
+      }
+      assert(a.size() < b.size());
+      return std::make_pair(q, normalize(a));
     }
 };
-template < class Mul, class Add, class Value >
-auto make_polynominal(Mul&& mul_fn, Add&& add_fn, Value zero)
-  { return polynominal< Mul, Add, Value >(std::forward< Mul >(mul_fn), std::forward< Add >(add_fn), zero); }
+constexpr auto not_implemented = [](auto const x, auto const) {
+  static_assert([]{ return false; }(), "Call to a not-implemented function." );
+  return x;
+};
+using not_implemented_t = std::decay_t< decltype(not_implemented) >;
+template < class Mul, class Add, class Value, class Div = not_implemented_t, class Sub = not_implemented_t >
+auto make_polynominal(
+  Mul const& mul_fn,
+  Add const& add_fn,
+  Value zero,
+  Div const& div_fn = not_implemented,
+  Sub const& sub_fn = not_implemented) {
+  return polynominal< Mul, Add, Value, Div, Sub >(mul_fn, add_fn, zero, div_fn, sub_fn);
+}
--- test/semirings/polynominal-test.cpp
+++ test/semirings/polynominal-test.cpp
@@ -1,6 +1,5 @@
 #include <catch2/catch.hpp>
 #include <bits/stdc++.h>
-#include <semirings/polynominal.hpp>
 #define loop(n) for (int ngtkana_is_genius = 0; ngtkana_is_genius < int(n); ngtkana_is_genius++)
 #define rep(i, begin, end) for(int i = int(begin); i < int(end); i++)
 #define all(v) v.begin(), v.end()
@@ -23,53 +22,261 @@ void debug_impl(Head head, Tail... tail){
 #else
 #define debug 0;
 #endif
+#include <semirings/polynominal.hpp>
 
-template < typename Container, typename Value = typename Container::value_type, std::enable_if_t<!std::is_same< Container, std::string >::value, std::nullptr_t> = nullptr>
-std::istream& operator>> (std::istream& is, Container& v)
-  { for (auto & x : v) { is >> x; } return is; }
+#define NORMALIZE(v, x) while (!v.empty() && v.back() == x) { v.pop_back(); }
 
-template < typename Container, typename Value = typename Container::value_type, std::enable_if_t<!std::is_same< Container, std::string >::value, std::nullptr_t> = nullptr >
-std::ostream& operator<< (std::ostream& os, Container const& v) {
- os << "{";
-  for (auto it = v.begin(); it != v.end(); it++)
-    {os << (it != v.begin() ? "," : "") << *it;}
-  return os << "}";
+#define SUB_DIV_TEST \
+  do {\
+      REQUIRE(poly.sub(c, a) == poly.normalize(b));\
+      REQUIRE(poly.sub(c, b) == poly.normalize(a));\
+      std::vector< TestType > q, r;\
+      poly.normalize(a), poly.normalize(b);\
+      if (!a.empty()) {\
+        std::tie(q, r) = poly.div(d, a);\
+        REQUIRE(q == poly.normalize(b));\
+        REQUIRE(r.empty());\
+      }\
+      if (!b.empty()) {\
+        std::tie(q, r) = poly.div(d, b);\
+        REQUIRE(q == poly.normalize(a));\
+        REQUIRE(r.empty());\
+      }\
+    }\
+  } while (false)
+
+template <typename T>
+T inverse(T a, T m) {
+  T u = 0, v = 1;
+  while (a != 0) {
+    T t = m / a;
+    m -= t * a; std::swap(a, m);
+    u -= t * v; std::swap(u, v);
+  }
+  assert(m == 1);
+  return u;
 }
+template <typename T>
+class modular {
+  private:
+    int value;
+  public:
+    constexpr modular() = default;
+    constexpr modular(const modular&) = default;
+    constexpr modular(modular&&) = default;
+    modular& operator=(const modular&) = default;
+    modular& operator=(modular&&) = default;
+
+    template <typename U>
+    modular (const U& x) {value = normalize(x);}
+
+    template <typename U>
+    static auto normalize(const U& x) {
+      int v = static_cast<int>(-mod() <= x && x < mod() ? x : x % mod());
+      if (v < 0) v += mod();
+      return v;
+    }
+
+    auto const& operator()() const { return value; }
+    template <typename U>
+    explicit operator U() const { return static_cast<U>(value); }
+    constexpr static auto mod() { return T::value; }
+
+    auto& operator+=(const modular& other) {
+      if ((value += other.value) >= mod()) value -= mod();
+      return *this;
+    }
+    auto& operator-=(const modular& other) {
+      if ((value -= other.value) < 0) value += mod();
+      return *this;
+    }
+    template <typename U>
+    auto& operator+=(const U& other) {return *this += modular(other); }
+    template <typename U>
+    auto& operator-=(const U& other) {return *this -= modular(other); }
+    auto operator-() const { return modular(-value); }
+    auto& operator++() {return *this += 1;}
+    auto& operator--() {return *this -= 1;}
+    auto  operator++(int) {modular result(*this); operator++(); return result;}
+    auto  operator--(int) {modular result(*this); operator--(); return result;}
+
+    template <typename U = T>
+    auto& operator*=(const modular& rhs) {
+      value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+      return *this;
+    }
+    auto& operator/=(const modular& other) {
+      return *this *= modular(inverse(other.value, mod()));
+    }
+};
+template <typename T> struct is_modular : std::false_type {};
+template <typename T> struct is_modular <modular<T>> : std::true_type{};
+template <typename T> constexpr bool is_modular_v = is_modular<T>::value;
+
+template <typename T> bool operator==(const modular<T>& lhs, const modular<T>& rhs) { return lhs() == rhs(); }
+template <typename T, typename U> bool operator==(const modular<T>& lhs, U rhs) { return lhs == modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const modular<T>& rhs) { return modular<T>(lhs) == rhs; }
+
+template <typename T> bool operator!=(const modular<T>& lhs, const modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const modular<T>& rhs) { return !(lhs == rhs); }
+
+template <typename T> modular<T> operator+(const modular<T>& lhs, const modular<T>& rhs) { return modular<T>(lhs) += rhs; }
+template <typename T, typename U> modular<T> operator+(const modular<T>& lhs, U rhs) { return modular<T>(lhs) += rhs; }
+template <typename T, typename U> modular<T> operator+(U lhs, const modular<T>& rhs) { return modular<T>(lhs) += rhs; }
+
+template <typename T> modular<T> operator-(const modular<T>& lhs, const modular<T>& rhs) { return modular<T>(lhs) -= rhs; }
+template <typename T, typename U> modular<T> operator-(const modular<T>& lhs, U rhs) { return modular<T>(lhs) -= rhs; }
+template <typename T, typename U> modular<T> operator-(U lhs, const modular<T>& rhs) { return modular<T>(lhs) -= rhs; }
+
+template <typename T> modular<T> operator*(const modular<T>& lhs, const modular<T>& rhs) { return modular<T>(lhs) *= rhs; }
+template <typename T, typename U> modular<T> operator*(const modular<T>& lhs, U rhs) { return modular<T>(lhs) *= rhs; }
+template <typename T, typename U> modular<T> operator*(U lhs, const modular<T>& rhs) { return modular<T>(lhs) *= rhs; }
+
+template <typename T> modular<T> operator/(const modular<T>& lhs, const modular<T>& rhs) { return modular<T>(lhs) /= rhs; }
+template <typename T, typename U> modular<T> operator/(const modular<T>& lhs, U rhs) { return modular<T>(lhs) /= rhs; }
+template <typename T, typename U> modular<T> operator/(U lhs, const modular<T>& rhs) { return modular<T>(lhs) /= rhs; }
+
+template<typename T, typename U>
+modular<T> power (const modular<T>& a, U b) {
+  assert(b >= 0);
+  modular<T> x = a, ret = 1;
+  for (; b > 0; b /= 2) {
+    if (b % 2 == 1) ret *= x;
+    x *= x;
+  }
+  return ret;
+}
+
+template <typename T>
+std::string to_string(const modular<T>& a) {
+  return std::to_string(a());
+}
+template <typename T>
+auto operator<<(std::ostream& os, const T& a)
+  -> std::enable_if_t<is_modular_v<T>, std::ostream&>{
+    return os << a();
+  }
+template <typename T>
+auto operator>>(std::istream& is, T& a)
+  -> std::enable_if_t<is_modular_v<T>, std::istream&> {
+  long long x; is >> x;
+  a = T(x);
+  return is;
+}
+
 
 TEMPLATE_TEST_CASE( "polynominal", "[polynominal]", int, long long ) {
   auto inf = std::numeric_limits< TestType >::max();
 
-  SECTION( "integral ring" ) {
+  SECTION( "mint field" ) {
+    constexpr TestType mod = 93;
+    using mint = modular< std::integral_constant< TestType, mod > >;
     auto const poly = make_polynominal(
       [](auto x, auto y){ return x * y; },
       [](auto x, auto y){ return x + y; },
-      TestType{0}
+      mint{0},
+      [](auto x, auto y){ return x / y; },
+      [](auto x, auto y){ return x - y; }
+    );
+    auto const deg_max = 20;
+    loop(72) {
+      auto const k = rand(1, deg_max), l = rand(1, deg_max);
+      auto const m = std::max(k, l), n = k + l - 1;
+      std::vector< mint > a(k), b(l);
+      for (auto & x : a) x = rand(0, mod - 1);
+      for (auto & y : b) y = rand(0, mod - 1);
+
+      // add test
+      auto c = std::vector< mint >(m, 0);
+      rep(i, 0, m) {
+        if (i < k) c.at(i) += a.at(i);
+        if (i < l) c.at(i) += b.at(i);
+      }
+      NORMALIZE(c, mint{0});
+      REQUIRE(poly.add(a, b) == c);
+
+      // mul test
+      auto d = std::vector< mint >(n, 0);
+      rep(i, 0, k) rep(j, 0, l) {
+        d.at(i + j) += a.at(i) * b.at(j);
+      }
+      NORMALIZE(d, 0);
+      REQUIRE(poly.mul(a, b) == d);
+
+      // sub_test
+      REQUIRE(poly.sub(c, a) == poly.normalize(b));
+      REQUIRE(poly.sub(c, b) == poly.normalize(a));
+
+      // div_test
+      // std::vector< mint > q, r;
+      // poly.normalize(a), poly.normalize(b);
+      // if (!a.empty()) {
+      //   std::tie(q, r) = poly.div(d, a);
+      //   REQUIRE(q == poly.normalize(b));
+      //   REQUIRE(r.empty());
+      // }
+      // if (!b.empty()) {
+      //   std::tie(q, r) = poly.div(d, b);
+      //   REQUIRE(q == poly.normalize(a));
+      //   REQUIRE(r.empty());
+      // }
+    }
+  }
+
+  SECTION( "int ring (and secure division)" ) {
+    auto const poly = make_polynominal(
+      [](auto x, auto y){ return x * y; },
+      [](auto x, auto y){ return x + y; },
+      TestType{0},
+      [](auto x, auto y){ return x / y; },
+      [](auto x, auto y){ return x - y; }
     );
     auto const deg_max = 20;
     auto const coeff_max = std::sqrt(inf) / deg_max;
-    loop(24) {
+    loop(72) {
       auto const k = rand(1, deg_max), l = rand(1, deg_max);
       auto const m = std::max(k, l), n = k + l - 1;
       std::vector< TestType > a(k), b(l);
       for (auto & x : a) x = rand(-coeff_max, coeff_max);
       for (auto & y : b) y = rand(-coeff_max, coeff_max);
-      auto const c = poly.add(a, b);
-      auto const d = poly.mul(a, b);
 
       // add test
-      auto c_tester = std::vector< TestType >(m, 0);
+      auto c = std::vector< TestType >(m, 0);
       rep(i, 0, m) {
-        if (i < k) c_tester.at(i) += a.at(i);
-        if (i < l) c_tester.at(i) += b.at(i);
+        if (i < k) c.at(i) += a.at(i);
+        if (i < l) c.at(i) += b.at(i);
       }
-      REQUIRE(c == c_tester);
+      while (c.empty() && c.back() == TestType{0})
+        { c.pop_back(); }
+      NORMALIZE(c, TestType{0});
+      REQUIRE(poly.add(a, b) == c);
 
       // mul test
-      auto d_tester = std::vector< TestType >(n, 0);
+      auto d = std::vector< TestType >(n, 0);
       rep(i, 0, k) rep(j, 0, l) {
-        d_tester.at(i + j) += a.at(i) * b.at(j);
+        d.at(i + j) += a.at(i) * b.at(j);
       }
-      REQUIRE(d == d_tester);
+      NORMALIZE(d, 0);
+      REQUIRE(poly.mul(a, b) == d);
+
+      // sub_test
+      REQUIRE(poly.sub(c, a) == poly.normalize(b));
+      REQUIRE(poly.sub(c, b) == poly.normalize(a));
+
+      // div_test
+      std::vector< TestType > q, r;
+      poly.normalize(a), poly.normalize(b);
+      if (!a.empty()) {
+        std::tie(q, r) = poly.div(d, a);
+        REQUIRE(q == poly.normalize(b));
+        REQUIRE(r.empty());
+      }
+      if (!b.empty()) {
+        std::tie(q, r) = poly.div(d, b);
+        REQUIRE(q == poly.normalize(a));
+        REQUIRE(r.empty());
+      }
     }
   }
 
@@ -92,36 +299,36 @@ TEMPLATE_TEST_CASE( "polynominal", "[polynominal]", int, long long ) {
       std::vector< TestType > a(k), b(l);
       for (auto & x : a) x = rand(-coeff_max, coeff_max);
       for (auto & y : b) y = rand(-coeff_max, coeff_max);
-      auto const min_add_result = min_poly.add(a, b);
-      auto const min_mul_result = min_poly.mul(a, b);
-      auto const max_add_result = max_poly.add(a, b);
-      auto const max_mul_result = max_poly.mul(a, b);
 
       // add test
-      std::vector< TestType > min_add_tester(m, inf);
-      std::vector< TestType > max_add_tester(m, -inf);
+      std::vector< TestType > c(m, inf);
+      std::vector< TestType > d(m, -inf);
       rep(i, 0, m) {
         if (i < k) {
-          cmn(min_add_tester.at(i), a.at(i));
-          cmx(max_add_tester.at(i), a.at(i));
+          cmn(c.at(i), a.at(i));
+          cmx(d.at(i), a.at(i));
         }
         if (i < l) {
-          cmn(min_add_tester.at(i), b.at(i));
-          cmx(max_add_tester.at(i), b.at(i));
+          cmn(c.at(i), b.at(i));
+          cmx(d.at(i), b.at(i));
         }
       }
-      REQUIRE(min_add_result == min_add_tester);
-      REQUIRE(max_add_result == max_add_tester);
+      NORMALIZE(c, inf);
+      NORMALIZE(d, inf);
+      REQUIRE(min_poly.add(a, b) == c);
+      REQUIRE(max_poly.add(a, b) == d);
 
       // mul test
-      std::vector< TestType > min_mul_tester(n, inf);
-      std::vector< TestType > max_mul_tester(n, -inf);
+      std::vector< TestType > e(n, inf);
+      std::vector< TestType > f(n, -inf);
       rep(i, 0, k) rep(j, 0, l) {
-        cmn(min_mul_tester.at(i + j), a.at(i) + b.at(j));
-        cmx(max_mul_tester.at(i + j), a.at(i) + b.at(j));
+        cmn(e.at(i + j), a.at(i) + b.at(j));
+        cmx(f.at(i + j), a.at(i) + b.at(j));
       }
-      REQUIRE(min_mul_result == min_mul_tester);
-      REQUIRE(max_mul_result == max_mul_tester);
+      NORMALIZE(e, inf);
+      NORMALIZE(f, inf);
+      REQUIRE(min_poly.mul(a, b) == e);
+      REQUIRE(max_poly.mul(a, b) == f);
     }
   }
 
@@ -147,11 +354,6 @@ TEMPLATE_TEST_CASE( "polynominal", "[polynominal]", int, long long ) {
       std::vector< TestType > a(k), b(l);
       for (auto & x : a) x = rand(0, coeff_max);
       for (auto & y : b) y = rand(0, coeff_max);
-      auto const gcd_add_result = gcd_poly.add(a, b);
-      auto const gcd_mul_result = gcd_poly.mul(a, b);
-      auto const lcm_add_result = lcm_poly.add(a, b);
-      auto const lcm_mul_result = lcm_poly.mul(a, b);
-      debug(a, b, gcd_add_result, lcm_add_result);
 
       // lambdas
       auto gcd_eq = [](auto& x, auto y){ x = std::__gcd(x, y); };
@@ -161,30 +363,66 @@ TEMPLATE_TEST_CASE( "polynominal", "[polynominal]", int, long long ) {
       };
 
       // add test
-      std::vector< TestType > gcd_add_tester(m, 0);
-      std::vector< TestType > lcm_add_tester(m, 1);
+      std::vector< TestType > c(m, 0);
+      std::vector< TestType > d(m, 1);
       rep(i, 0, m) {
         if (i < k) {
-          gcd_eq(gcd_add_tester.at(i), a.at(i));
-          lcm_eq(lcm_add_tester.at(i), a.at(i));
+          gcd_eq(c.at(i), a.at(i));
+          lcm_eq(d.at(i), a.at(i));
         }
         if (i < l) {
-          gcd_eq(gcd_add_tester.at(i), b.at(i));
-          lcm_eq(lcm_add_tester.at(i), b.at(i));
+          gcd_eq(c.at(i), b.at(i));
+          lcm_eq(d.at(i), b.at(i));
         }
       }
-      REQUIRE(gcd_add_result == gcd_add_tester);
-      REQUIRE(lcm_add_result == lcm_add_tester);
+      NORMALIZE(c, TestType{0});
+      NORMALIZE(d, TestType{1});
+      REQUIRE(gcd_poly.add(a, b) == c);
+      REQUIRE(lcm_poly.add(a, b) == d);
 
       // mul test
-      std::vector< TestType > gcd_mul_tester(n, 0);
-      std::vector< TestType > lcm_mul_tester(n, 1);
+      std::vector< TestType > e(n, 0);
+      std::vector< TestType > f(n, 1);
       rep(i, 0, k) rep(j, 0, l) {
-        gcd_eq(gcd_mul_tester.at(i + j), a.at(i) * b.at(j));
-        lcm_eq(lcm_mul_tester.at(i + j), a.at(i) * b.at(j));
+        gcd_eq(e.at(i + j), a.at(i) * b.at(j));
+        lcm_eq(f.at(i + j), a.at(i) * b.at(j));
       }
-      REQUIRE(gcd_mul_result == gcd_mul_tester);
-      REQUIRE(lcm_mul_result == lcm_mul_tester);
+      NORMALIZE(e, TestType{0});
+      NORMALIZE(f, TestType{1});
+      REQUIRE(gcd_poly.mul(a, b) == e);
+      REQUIRE(lcm_poly.mul(a, b) == f);
+    }
+  }
+
+  SECTION( "and-xor ring" ) {
+    auto const poly = make_polynominal(
+      [](auto x, auto y){ return x & y; },
+      [](auto x, auto y){ return x ^ y; },
+      TestType{0}
+    );
+    auto const deg_max = 40;
+    loop(24) {
+      auto const k = rand(1, deg_max), l = rand(1, deg_max);
+      std::vector< TestType > a(k), b(l);
+      for (auto & x : a) x = rand(0, inf);
+      for (auto & y : b) y = rand(0, inf);
+
+      // add test
+      std::vector< TestType > c(std::max(k, l), 0);
+      rep(i, 0, std::max(k, l)) {
+        if (i < k) { c.at(i) ^= a.at(i); }
+        if (i < l) { c.at(i) ^= b.at(i); }
+      }
+      NORMALIZE(c, TestType{0});
+      REQUIRE(poly.add(a, b) == c);
+
+      // mul test
+      std::vector< TestType > d(k + l - 1, 0);
+      rep(i, 0, k) rep(j, 0, l) {
+        d.at(i + j) ^= a.at(i) & b.at(j);
+      }
+      NORMALIZE(c, TestType{0});
+      REQUIRE(poly.mul(a, b) == d);
     }
   }
 }
