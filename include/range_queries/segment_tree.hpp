bb017256b2580c0668ea309f3b31c52107b2ad36
ngtkana <toshiki_tanaka@caddi.jp>
2019-10-20 23:35:05 +0900

    fixed segtree and hld

--- include/range_queries/segment_tree.hpp
+++ include/range_queries/segment_tree.hpp
@@ -1,25 +1,14 @@
-template<typename Value, typename BinaryOp>
+template < typename Value, typename BinaryOp >
 class segment_tree {
-    int                 sz, n, N;
-    BinaryOp            op;
-    Value               id;
-    std::vector<Value>  table;
+    int                  sz, n, N;
+    BinaryOp             op;
+    Value                id;
+    std::vector< Value > table;
 
-    auto& op_eq(Value& x, Value y) const { return x = op(x, y); }
-
+    auto& op_eq(Value& x, Value y) const
+      { return x = op(x, y); }
     void merge(int u)
       { table.at(u) = op(table.at(2 * u), table.at(2 * u + 1)); }
-
-    Value query_impl(int l, int r, int k, int L, int R) const {
-      return l <= L && R <= r
-        ? table.at(k)
-        : R <= l || r <= L
-        ? id
-        : op(
-          query_impl(l, r, 2 * k, L, (L + R) / 2),
-          query_impl(l, r, 2 * k + 1, (L + R) / 2, R)
-        );
-    }
 
   public:
     segment_tree(int sz, BinaryOp op, Value id):
@@ -32,34 +21,30 @@ class segment_tree {
       {}
 
     auto& at(int i)       { return table.at(n + i); }
-
     auto& at(int i) const { return table.at(n + i); }
-
     auto collect() const {
       auto ret = std::vector<Value>(sz);
       for (auto i = 0; i < sz; i++)
         { ret.at(i) = at(i); }
       return ret;
     }
-
-    auto query(int l, int r) const
-      { return query_impl(l, r, 1, 0, n); }
-
+    auto query(int l, int r) const {
+      auto const dfs = [&](auto&& f, int l, int r, int k, int L, int R) -> Value {
+        return l <= L && R <= r
+          ? table.at(k)
+          : R <= l || r <= L
+          ? id
+          : op(
+            f(f, l, r, 2 * k, L, (L + R) / 2),
+            f(f, l, r, 2 * k + 1, (L + R) / 2, R)
+          );
+      };
+      return dfs(dfs, l, r, 1, 0, n);
+    }
     void build_oneline(int i)
       { for (i += n, i /= 2; i > 0; i /= 2) merge(i); }
-
     void build()
       { for (auto i = n - 1; i > 0; i--) merge(i); }
-
-    void update (int u, Value val) {
-      at(u) = val;
-      build_oneline(u);
-    }
-
-    void add (int u, Value val) {
-      at(u) += val;
-      build_oneline(u);
-    }
 };
 
 template<typename Value, typename BinaryOp>
--- include/tree/heavy_light_decomposition.hpp
+++ include/tree/heavy_light_decomposition.hpp
@@ -1,101 +1,67 @@
-class heavy_light_decomposition
-{
-  public:
-    using size_type  = int;
-    using index_type = int;
-    using id_type    = int;
-    using graph_type = std::vector<std::vector<index_type>>;
-    using tablele_type = std::vector<int>;
-
-  private:
-    size_type  n;
-    graph_type graph;
-    tablele_type prt, sz, depth, vid, ord, head;
-
-    // Helper
-    void first_dfs(index_type x)
-    {
-      for (auto& y : graph.at(x))
-      {
-        if (y == prt.at(x)) continue;
-        prt.at(y)   = x;
-        depth.at(y) = depth.at(x) + 1;
-
-        first_dfs(y);
-
-        sz.at(x) += sz.at(y);
-        auto& h = graph.at(x).front();
-        if (sz.at(h) < sz.at(y))
-        {
-          std::swap(h, y);
-        }
-      }
-    }
-
-    // Helper
-    void second_dfs(index_type x, id_type& counter)
-    {
-      ord.at(vid.at(x) = counter++) = x;
-      for (auto y : graph.at(x))
-      {
-        if (y == prt.at(x)) continue;
-        head.at(y) = y == graph.at(x).front()
-          ? head.at(x)
-          : y;
-         second_dfs(y, counter);
-      }
-    }
+class heavy_light_decomposition {
+    int n;
+    std::vector< std::vector< int > > graph;
+    std::vector < int > prt, sz, vid, ord, head;
 
   public:
     heavy_light_decomposition()=default;
-    heavy_light_decomposition(const heavy_light_decomposition&)=default;
-    heavy_light_decomposition(heavy_light_decomposition&&)=default;
-    heavy_light_decomposition& operator=(const heavy_light_decomposition&)=default;
-    heavy_light_decomposition& operator=(heavy_light_decomposition&&)=default;
-
     heavy_light_decomposition(int n) :
       n     (n),
       graph (n),
       prt   (n),
       sz    (n, 1),
-      depth (n, 0),
       vid   (n),
       ord   (n),
       head  (n)
       {}
 
     // Accessers.
-    auto id(index_type x) const {return vid.at(x);}
+    auto id(int x) const {return vid.at(x);}
 
-    void build (index_type root = 0)
-    {
-      id_type counter = 0;
-      first_dfs(root);
-      second_dfs(root, counter);
+    void build (int root = 0) {
+      auto const dfs = [&](auto&& f , auto crr) -> void {
+        for (auto& nxt : graph.at(crr)) {
+          if (nxt == prt.at(crr)) continue;
+          prt.at(nxt) = crr;
+          f(f, nxt);
+          sz.at(crr) += sz.at(nxt);
+          auto& h = graph.at(crr).front();
+          if (sz.at(h) < sz.at(nxt))
+            { std::swap(h, nxt); }
+        }
+      };
+      dfs(dfs, root);
+
+      auto vm = 0;
+      auto const efs = [&](auto&& f, auto crr) -> void {
+        ord.at(vid.at(crr) = vm++) = crr;
+        for (auto nxt : graph.at(crr)) {
+          if (nxt == prt.at(crr)) continue;
+          head.at(nxt) = nxt == graph.at(crr).front() ? head.at(crr) : nxt;
+          f(f, nxt);
+        }
+      };
+      efs(efs, root);
     }
 
-    void insert (index_type x, index_type y)
-    {
+    void insert (int x, int y) {
       graph.at(x).push_back(y);
       graph.at(y).push_back(x);
     }
 
-    template <typename RangeOp>
-    void for_each
-    (
-      index_type     x,
-      index_type     y,
+    template < class RangeOp >
+    void for_each (
+      int x,
+      int y,
       RangeOp const& op,   // range-operation f([vid(x), vid(y)[)
-      bool           skip  // skip the top vertex
+      bool skip            // skip the top vertex
     )
     {
-      while (true)
-      {
+      while (true) {
         if (id(x) > id(y)) std::swap(x, y);
-        if (head.at(x) == head.at(y))
-        {
+        if (head.at(x) == head.at(y)) {
           op(id(x) + skip, id(y) + 1);
-          break;
+          return;
         }
         op(id(head.at(y)), id(y) + 1);
         y = prt.at(head.at(y));
